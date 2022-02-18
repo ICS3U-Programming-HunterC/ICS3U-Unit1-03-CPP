42e6941c1c216ecc2689fc797655b85def2a11d9
Ubuntu <ubuntu@ip-172-31-3-2.ec2.internal>
2022-02-18 03:28:02 +0000

    added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Hunter Connolly All rights reserved
+//
+// Created by: Hunter Connolly
+// February 17, 2022
+// This is a "Hello world" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! with proper style" << std::endl;
+}
